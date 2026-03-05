0f803dc50ee91864d82788882fd8f12e8c4d2f19
rahel-yab <rahelyabebal@gmail.com>
2026-03-05 19:43:10 +0300

    defined tasks table

---
+++ database/migrations/2026_03_05_164152_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('project_id')->constrained()->onDelete('cascade');
+        // Relationship: We point 'assigned_to' specifically to the users table
+        $table->foreignId('assigned_to')->constrained('users')->onDelete('cascade');
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->string('priority')->default('medium'); // low, medium, high
+        $table->string('status')->default('todo'); // todo, in_progress, done
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
