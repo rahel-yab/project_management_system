41ce2ba74d16327443c2aa709987029d5af621e4
rahel-yab <rahelyabebal@gmail.com>
2026-03-06 11:02:47 +0300

    added comment functionality

---
+++ app/Models/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['task_id', 'user_id', 'content'];
+
+    public function task() { return $this->belongsTo(Task::class); }
+    public function user() { return $this->belongsTo(User::class); }
+}
