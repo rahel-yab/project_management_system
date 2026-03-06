8ed7d2f0715a2483b8a741051cd516e9d3ec1f82
rahel-yab <rahelyabebal@gmail.com>
2026-03-06 12:04:11 +0300

    added auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// protected routes
 Route::middleware('auth:sanctum')->group(function () {
+    
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     // --- PROJECT ROUTES ---
     Route::get('/projects', [ProjectController::class, 'index']);
@@ -17,7 +27,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::post('/tasks', [TaskController::class, 'store'])->middleware('role:manager');
     Route::get('/tasks/{task}', [TaskController::class, 'show']);
-    // Status update (The one we protected with a Policy)
     Route::patch('/tasks/{task}/status', [TaskController::class, 'updateStatus']);
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 
