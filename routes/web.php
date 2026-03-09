49961b2ba80fbf2d54ec17ce6a914dfb9d374a06
rahel-yab <rahelyabebal@gmail.com>
2026-03-09 08:54:16 +0300

    added reset-password route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
+
+Route::get('/reset-password/{token}', function (Request $request, string $token) {
+	return response()->json([
+		'message' => 'Use this token and email to call POST /api/reset-password.',
+		'token' => $token,
+		'email' => $request->query('email'),
+	]);
+})->name('password.reset');
